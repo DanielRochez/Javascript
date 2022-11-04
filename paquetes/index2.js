828931863b99349826660a8db2f24237a030458f
DanielRochez <rochezsalgado@gmail.com>
2022-11-04 17:50:11 -0600

    index2

---
+++ paquetes/index2.js
@@ -0,0 +1,11 @@
+var app = express();
+app.get('/', function(req,res){
+    res.send('Hola mundo');
+});
+app.get('/saludo', function(req, res){
+    res.send('Solicitud de saludo recibida');
+});
+
+app.listen(8000,function(){
+    console.log('Servidor corriendo en el puerto 8000');
+});
